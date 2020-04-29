67164e72b6b3bbc7833df75c2eaf8f9a2e6bf4ed
campeon19 <53914171+campeon19@users.noreply.github.com>
2020-04-29 10:26:52 -0600

    Primer Commit

---
+++ Ejercicio15.s
@@ -0,0 +1,37 @@
+/*
+* Ejercicio 15.s
+*
+* USO: Practica de SWI
+*
+* AUTH: Christian Perez
+*
+*/
+
+/* Datos */
+
+.data
+
+string:		.asciz "                                                                                                                      "
+enter:		.asciz "\n"
+
+/*PROGRAMA*/
+
+.global main
+
+main:
+
+@@ LEER CARACTERES
+
+	mov r7, #3
+	mov r0, #0
+	mov r2, #129
+	ldr r1,=string
+	swi 0
+	
+@@ Mustra la cadena leida
+	mov r7, #4
+	mov r0, #1
+	mov r2, #
+	
+	
+	
